fb36f583041439cb557aa12e68598fd8840f23b7
GabrielyPaz <gabrielysouza120@gmail.com>
2023-03-12 20:00:14 -0300

    atualizando arquivos

--- Front-End-II/Check-point-I/main.js
+++ Front-End-II/Check-point-I/main.js
@@ -51,17 +51,15 @@ for (let item of itens) {
     `
 
 }
-const campos = [
-
-    "titulo", "url", "descricao"
-
-]
+const campos = ["titulo", "url", "descricao"]
 
 let formulario = document.querySelector(".Cadastro-form");
+
 formulario.addEventListener("submit", function(event) {
     console.log("Cadastro realizado com sucesso!")
     console.log(event)
-   event.preventDefault()
+
+    event.preventDefault()
 
    for(let campo of campos) {
     const tituloField = document.getElementById(campo);
@@ -79,3 +77,160 @@ formulario.addEventListener("submit", function(event) {
    return valid;
 
 })
+
+const tituloRef = document.querySelector('#titulo');
+const urlImgRef = document.querySelector('#url');
+const descricaoRef = document.querySelector('#descricao');
+const botaoPostarRef = document.querySelector('#postar') ;
+
+let teste = [];
+
+class Postage
+{
+    constructor() 
+    {
+        this.id = 1;
+        this.arrayPostagem = 
+        [/*
+            {
+                urlImg: './imagens/godfrey.png',
+                titulo: 'Godfrey, the first Elden Lord ',
+                descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, eveniet, ipsa soluta debitis sed consectetur temporibus fugit maiores eum labore explicabo voluptates doloremque illo accusantium eius cupiditate aliquid amet facilis!'
+            },
+            
+            {
+                urlImg: './imagens/malenia.png',
+                titulo: 'Malenia Blade of Miquella',
+                descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, eveniet, ipsa soluta debitis sed consectetur temporibus fugit maiores eum labore explicabo voluptates doloremque illo accusantium eius cupiditate aliquid amet facilis!'
+            },
+        
+            {
+                urlImg: './imagens/starscourge.png',
+                titulo: 'Starscourge Radahn',
+                descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, eveniet, ipsa soluta debitis sed consectetur temporibus fugit maiores eum labore explicabo voluptates doloremque illo accusantium eius cupiditate aliquid amet facilis!'    
+            },
+        
+            {
+                urlImg: './imagens/ranni.png',
+                titulo: 'Ranni the Witch',
+                descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, eveniet, ipsa soluta debitis sed consectetur temporibus fugit maiores eum labore explicabo voluptates doloremque illo accusantium eius cupiditate aliquid amet facilis!'
+            },
+        
+            {
+                urlImg: './imagens/ballid.png',
+                titulo: 'Balidd',
+                descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, eveniet, ipsa soluta debitis sed consectetur temporibus fugit maiores eum labore explicabo voluptates doloremque illo accusantium eius cupiditate aliquid amet facilis!'
+            },
+        
+            {
+                urlImg: './imagens/mohg.png',
+                titulo: 'Mohg, Lord of Blood',
+                descricao: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, eveniet, ipsa soluta debitis sed consectetur temporibus fugit maiores eum labore explicabo voluptates doloremque illo accusantium eius cupiditate aliquid amet facilis!'
+            }*/
+    
+        ]
+        //this.titulo = titulo;
+        //this.urlImg = urlImg;
+        //this.descricao = descricao;
+
+    }
+
+    salvarPostagem()
+    {
+        let novoPost = this.lerDados();
+
+        if(this.validaCampos(novoPost))
+        {
+           this.adicionarPostagem(novoPost)
+        }
+
+        this.listaPostagens();
+
+        this.cancelar();
+      
+       // console.log(this.arrayPostagem);
+    }
+
+    listaPostagens()
+    {
+        let containerRef = document.querySelector('.posts');
+        containerRef.innerHTML = '';
+
+        for (let item of this.arrayPostagem) 
+        {
+
+            containerRef.innerHTML += 
+            `
+                <div class="item">
+                    <img src="${item.urlImg}">
+                    <h2>${item.titulo}</h2>
+                    <p>${item.descricao}</p>
+                </div>
+            `
+
+        };
+    }
+
+    adicionarPostagem(novoPost)
+    {
+        this.arrayPostagem.unshift(novoPost);
+        this.id++;
+    }
+
+    lerDados()
+    {
+        let novoPost = {};
+        novoPost.id = this.id
+        novoPost.titulo = document.querySelector('#titulo').value;
+        novoPost.urlImg = document.querySelector('#url').value;
+        novoPost.descricao = document.querySelector('#descricao').value;
+
+        return novoPost
+    }
+
+    // login (event)
+    // {
+    //     event.preventDefault();
+    // }
+
+    // validaCampos(novoPost)
+    // {
+    //     let msg = '';
+
+
+    //     if(novoPost.titulo == '')
+    //     {
+    //         msg += '- Preencha o titulo da postagem \n';
+    //     }
+
+    //     if(novoPost.urlImg == '')
+    //     {
+    //         msg += '- Preencha o endereço da imagem \n';
+    //     }
+
+    //     if(novoPost.descricao == '')
+    //     {
+    //         msg += '- Preencha a descrição da postagem\n';
+    //     }
+
+    //     if(msg != '')
+    //     {
+    //         alert(msg);
+    //         return false;
+    //     }
+    //     return true
+    // }
+
+    // cancelar()
+    // {
+    //     document.querySelector('#titulo').value = '';
+    //     document.querySelector('#url').value = '';
+    //     document.querySelector('#descricao').value = '';
+    // }
+}
+
+let novoPost = new Postage();
+
+document.querySelector('#postar').addEventListener('click', (event => novoPost.login(event)));
+
+document.querySelector('#postar').addEventListener('click', (event) => novoPost.salvarPostagem(event));
